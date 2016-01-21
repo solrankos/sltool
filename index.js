afb34146d39b174e8c17bb0308040396b535d988
Oscar Karlsson <oscar.karlsson@apegroup.com>
2016-01-21 12:35:32 +0100

    Added request module. Also added a request for to the SL platsuppslag API

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 #! /usr/bin/env node
 
-var args = process.argv.slice(2);
+// modules
+var request = require('request')
 
-console.log(args);
+// arguments
+var arg = process.argv.slice(2)
+
+// globals
+var APIKeys = {
+	SLPlatsupplag: '97b236dee34c4412a743f61af4dba37f',
+	SLRealtid3: '80d16ebb1c2a4bc3a72e9cd9be029b99'
+}
+
+
+
+function getSubwaySites(searchString) {
+	var getSubwaySitesEndpoint = 'https://api.sl.se/api2/typeahead.json'
+	.concat('?key=' + APIKeys.SLPlatsupplag)
+	.concat('&searchstring=' + searchString)
+	.concat('&maxResults=1')
+
+	request(getSubwaySitesEndpoint, function(error, res, body) {
+		if (error) {
+			console.log(error)
+			return
+		} 
+		if (res.statusCode != 200) {
+			console.log(res.statusCode)
+			return
+		}
+		if (!body['ResponseData']) {
+			console.log('No response data')
+		}
+
+		console.log(body['ResponseData'])
+	})
+
+	console.log(getSubwaySitesEndpoint)
+}
+
+getSubwaySites(arg)
