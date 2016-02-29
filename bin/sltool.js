11b0be7be2d8ff1c3ebf086834ac6c3e6e39f6b2
Oscar Karlsson <oscar.karlsson@apegroup.com>
2016-02-29 22:02:48 +0100

    Massive refactoring
    Moved a lot of functions to spearate files

--- bin/sltool.js
+++ bin/sltool.js
@@ -2,23 +2,21 @@
 "use strict";
 
 // extern modules
-var request = require('request');
 var Spinner = require('cli-spinner').Spinner;
-var chalk = require('chalk');
-var prompt = require('prompt');
-var version = require('version');
 
 // internal modules
-var printer = require('./../lib/printer.js');
-var globals = require('./../lib/globals.js');
+var printer = requireInternalModule('printer');
+var prompter = requireInternalModule('prompter');
+var globals = requireInternalModule('globals');
+var version = requireInternalModule('version');
+var sl = requireInternalModule('sl-api');
+
+function requireInternalModule(module) {
+    return require('./../lib/' + module + ".js");
+}
 
 // globals
-var gAPIKeys = {
-    SLPlatsupplag: '97b236dee34c4412a743f61af4dba37f',
-    SLRealtid3: '80d16ebb1c2a4bc3a72e9cd9be029b99'
-};
 var gSpinner = getSpinner();
-
 var args = process.argv.slice(2);
 
 parseArguments(args);
@@ -27,7 +25,7 @@ function parseArguments(args) {
     if (args.length == 0 || arrayContainKeys(args, ['-h', '--help'])) {
         printer.printHelp();
     } else {
-        getSubwaySites(args);
+        getInfo(args);
     }
 }
 
@@ -45,127 +43,59 @@ function arrayContainKeys(arrayToSearch, arrayOfKeys) {
     return match;
 }
 
+function getInfo(args) {
+    gSpinner.start()
+    
+    sl.getSubwaySites(args, function(sites) {
+        gSpinner.stop(true);
+
+        if (sites.length > 1) {
+            printer.printSearchResults(sites);
+            promptUserForSearchResults(sites);
+        } else {
+            var site = sites[0];
+            getRealtimeInfo(site);
+        }
+
+    });
+}
+
+function promptUserForSearchResults(sites) {
+    prompter.promptUserForSearchResults(sites, function(err, site) {
+        if (err) {
+            printer.printUserInfo("You need to provide a valid number.");
+            promptUserForSearchResults(sites);
+        } else {
+            getRealtimeInfo(site);
+        }
+    });
+}
+
+function getRealtimeInfo(site) {
+    gSpinner.start();
+
+    sl.getRealtimeInfo(site, function(siteName, metros) {
+        gSpinner.stop(true)
+
+        if (metros.length == 0) {
+            printer.printUserInfo('No metros for ' + siteName);
+            return;
+        }
+
+        printer.printRealTimeInformation(siteName ,metros);
+        version.check();
+    });
+}
+
 function getSpinner() {
     var spinner = new Spinner();
     spinner.setSpinnerString(0);
     return spinner;
 }
 
-function getSubwaySites(searchString) {
-    gSpinner.start();
-
-    var getSubwaySitesEndpoint = 'https://api.sl.se/api2/typeahead.json'
-    .concat('?key=' + gAPIKeys.SLPlatsupplag)
-    .concat('&searchstring=' + searchString)
-    .concat('&StationsOnly=true')
-    .concat('&maxResults=5');
-
-    request(getSubwaySitesEndpoint, function(error, res, body) {
-        gSpinner.stop(true)
-
-        if (error) {
-            handleError(error);
-            return;
-        } 
-        if (res.statusCode != 200) {
-            handleError(res.statusCode);
-            return;
-        }
-
-        var obj = JSON.parse(body);
-
-        if (!obj.ResponseData) {
-            handleError('No site response data', obj);
-            return;
-        }
-
-        var sites = obj.ResponseData;
-
-        if (sites.length > 1) {
-            printer.printSearchResults(sites);
-            promptUserForSearchResults(sites);
-        } else {
-            var site = obj.ResponseData[0];
-            getRealtimeInfo(site);
-        }
-    })
-}
-
-function promptUserForSearchResults(searchResults) {
-    prompt.message = '';
-    prompt.delimiter = '';
-    prompt.start();
-    prompt.get({
-        properties: {
-            option: {
-                description: '>'.green
-            }
-        }
-    }, function(err, result) {
-        var option = result.option;
-        if (option < searchResults.length && option >= 0) {
-            var site = searchResults[option];
-            getRealtimeInfo(site);
-        } else {
-            printer.printUserInfo("You need to provide a valid number.");
-            promptUserForSearchResults(searchResults);
-        }
-    });
-}
-
-function getRealtimeInfo(site) {
-    gSpinner.start()
-
-    var getRealtimeInfoEndpoint = 'https://api.sl.se/api2/realtimedepartures.json'
-    .concat('?key=' + gAPIKeys.SLRealtid3)
-    .concat('&siteid=' + site.SiteId)
-    .concat('&timewindow=30');
-
-    if (globals.debug) {
-        console.log(getRealtimeInfoEndpoint);
-    }
-
-    request(getRealtimeInfoEndpoint, function(error, res, body) {
-        gSpinner.stop(true);
-
-        if (error) {
-            handleError(error);
-            return;
-        } 
-        if (res.statusCode != 200) {
-            handleError(res.statusCode);
-            return;
-        }
-
-        var obj = JSON.parse(body);
-
-        if (!obj.ResponseData || !obj.ResponseData.Metros) {
-            handleError('No realtime response data', obj);
-            return;
-        }
-
-        if (obj.ResponseData.Metros.length == 0) {
-            printer.printUserInfo('No metros for ' + site.Name);
-            return; 
-        }
-
-        printer.printRealTimeInformation(site.Name ,obj.ResponseData.Metros);
-
-        getVersion();
-    });
-}
-
-function getVersion() {
-    version.fetch('sltool', function(error, version) {
-        if (!error) {
-            printer.printUserInfo("Current version is: " + version);
-        };
-    });
-}
-
 function handleError(message, debugData) {
     if (globals.debug && debugData) {
         console.error("Debug data:\n" + debugData);
     }
-    console.error(chalk.red("ERR! ") + message);
+    printer.printError(message);
 }
