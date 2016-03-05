f10f99c6d681320172bb61cbe550074038ad70c2
Oscar Karlsson <oscar.karlsson@apegroup.com>
2016-03-05 20:43:04 +0100

    Now prints and filters buses and metros

--- bin/sltool.js
+++ bin/sltool.js
@@ -20,51 +20,83 @@ function requireInternalModule(module) {
 var gSpinner = getSpinner();
 var args = process.argv.slice(2);
 
-parseArguments(args);
+main(args);
 
-function parseArguments(args) {
-    if (args.length == 0 || helper.arrayContainKeys(args, ['-h', '--help'])) {
+function main(args) {
+    var options = parseOptions(args);
+    var searchString = parseSearchString(args)
+
+    if (searchString == null || helper.arrayContainKeys(options, ['help'])) {
         printer.printHelp();
     } else {
-        getInfo(args);
+        getInfo(searchString, options);
     }
 }
 
-function getInfo(args) {
+function parseOptions(args) {
+    var options = [];
+    if (helper.arrayContainKeys(args, ['-h', '--help'])) {
+        options.push("help");
+    }
+    if (helper.arrayContainKeys(args, ['-m', '--metros'])) {
+        options.push("metros");
+    }
+    if (helper.arrayContainKeys(args, ['-b', '--buses'])) {
+        options.push("buses");
+    }
+
+    return options;
+}
+
+function parseSearchString(args) {
+    var searchStrings = [];
+
+    for (var key in args) {
+        var arg = args[key];
+
+        if (arg.substring(0, 1) != "-") {
+            searchStrings.push(arg);
+        }
+    }
+
+    return searchStrings[0];
+}
+
+function getInfo(searchString, options) {
     gSpinner.start()
-    
+
     sl.getSites(args, function(sites) {
         gSpinner.stop(true);
 
         if (sites.length > 1) {
             printer.printSearchResults(sites);
-            promptUserForSearchResults(sites);
+            promptUserForSearchResults(sites, options);
         } else {
             var site = sites[0];
-            getRealtimeInfo(site);
+            getRealtimeInfo(site, options);
         }
 
     });
 }
 
-function promptUserForSearchResults(sites) {
+function promptUserForSearchResults(sites, options) {
     prompter.promptUserForSearchResults(sites, function(err, site) {
         if (err) {
             printer.printUserInfo(err);
-            promptUserForSearchResults(sites);
+            promptUserForSearchResults(sites, options);
         } else {
-            getRealtimeInfo(site);
+            getRealtimeInfo(site, options);
         }
     });
 }
 
-function getRealtimeInfo(site) {
+function getRealtimeInfo(site, options) {
     gSpinner.start();
 
     sl.getRealtimeInfo(site, function(siteName, response) {
         gSpinner.stop(true)
 
-        printer.printRealTimeInformation(siteName, response);
+        printer.printRealTimeInformation(siteName, response, options);
         version.check();
     });
 }
